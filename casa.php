d751b5c214d5115da836e9909b8ae00b13beffde
jmurillo106 <jmurillo106@gmail.com>
2016-07-11 00:29:20 -0700

    otro mas

--- casa.php
+++ casa.php
@@ -1,13 +1,26 @@
 <?PHP
+$salida="";
 $db=mysql_connect("localhost","localuser","localpass") or die(mysql_error());
+mysql_select_db("www");
+$result=mysql_query("select * from casa;");
+while($row=mysql_fetch_array($result)){
+	$salida.=<<<rect
+	<rect id="$row[1]" x="$row[2]" y="$row[3]" width="$row[4]" height="$row[5]" style="fill:blue;stroke-width:4;stroke:#000" >\n
+rect;
+}
 
-mysql_close($db);
-
-
-
-
+$salida="<svg width=\"400\" height=\"400\" >\n $salida </svg>";
 
 print<<<html
-1
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+ $salida 
+
+</body>
+</html>
 html;
 ?>
