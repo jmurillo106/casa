eb21b461002cfbdfae03d89e05024b66556276dd
jmurillo106 <jmurillo106@gmail.com>
2016-07-10 21:43:39 -0700

    actualizacion

---
+++ index.php
@@ -0,0 +1,34 @@
+<?PHP
+print<<<html
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+<script>
+$(document).ready(function(){
+	cargar();
+});
+
+function cargar(){
+}
+
+
+
+
+
+</script>
+
+
+</head>
+<body>
+<svg width=200 height=200>
+	<rect x=5 y=5 width=50 height=50 fill="red" stroke-width=4 stroke="red">
+</svg>
+
+
+
+</body>
+</html>
+html;
+
+?>
