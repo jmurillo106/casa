e3546ad8425fc409098d1182265ee4b35b54774c
jmurillo106 <jmurillo106@gmail.com>
2016-07-11 00:40:56 -0700

    update

--- casa.php
+++ casa.php
@@ -5,7 +5,7 @@ mysql_select_db("www");
 $result=mysql_query("select * from casa;");
 while($row=mysql_fetch_array($result)){
 	$salida.=<<<rect
-	<rect id="$row[1]" x="$row[2]" y="$row[3]" width="$row[4]" height="$row[5]" style="fill:blue;stroke-width:4;stroke:#000" >\n
+	<rect id="$row[1]" x="$row[2]" y="$row[3]" width="$row[4]" height="$row[5]" style="fill:white;stroke-width:0.5;stroke:#000" />\n
 rect;
 }
 
